a5874bce336d0d0b5276f099c3064d52030823be
arrow <alparslanu6347@gmail.com>
2023-03-12 21:13:57 +0300

    userdata updated

--- resource_ec2-instance.tf
+++ resource_ec2-instance.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "arrow_kittens-ec2" {
       yum update -y
       yum install httpd -y
       yum install wget -y
-      FOLDER="https://raw.githubusercontent.com/alparslanu6347/kittens/main/Project-101-kittens-carousel-static-website-ec2/static-web"
+      FOLDER="https://raw.githubusercontent.com/alparslanu6347/kittens-terraform/main/static-web"
       cd /var/www/html
       wget $FOLDER/index.html
       wget $FOLDER/cat0.jpg
