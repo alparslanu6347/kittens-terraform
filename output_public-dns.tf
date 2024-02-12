e8767b7796fd8643ed2729dbaf77ce6e1b93ffce
alparslanu6347 <alparslanu6347@gmail.com>
2024-02-12 08:53:31 +0300

    unnecessary outputs deleted

--- output_public-dns.tf
+++
@@ -1,4 +0,0 @@
-output "instance_publicdns" {     # Bu output kullaniyorsan """ count""" KULLANAMAZSIN HATA VERİYOR. Çünkü sadece 1 tane instance kullanıyorsun.
-  description = "EC2 Instance Public DNS"
-  value = aws_instance.arrow_kittens-ec2.public_dns   # value = aws_instance.arrow_kittens-ec2.*.public_dns  OLARAK YAZARSAN count  kullanabilirsin
-}
