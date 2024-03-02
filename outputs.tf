1e2c917154be2d1399eb55276db41ec2e191e2b1
alparslanu6347 <alparslanu6347@gmail.com>
2024-03-02 10:25:53 +0300

    unnecessary files deleted

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_publicip" {        # Bu output kullaniyorsan """ count""" KULLANAMAZSIN HATA VERİYOR. Çünkü sadece 1 tane instance kullanıyorsun. 
-  description = "EC2 Instance Public IP"
-  value = aws_instance.arrow_kittens-ec2.public_ip  # value = aws_instance.arrow_kittens-ec2.*.public_ip  OLARAK YAZARSAN count  kullanabilirsin
-}
-
-output "instance_publicdns" {     # Bu output kullaniyorsan """ count""" KULLANAMAZSIN HATA VERİYOR. Çünkü sadece 1 tane instance kullanıyorsun.
-  description = "EC2 Instance Public DNS"
-  value = aws_instance.arrow_kittens-ec2.public_dns   # value = aws_instance.arrow_kittens-ec2.*.public_dns  OLARAK YAZARSAN count  kullanabilirsin
-}
